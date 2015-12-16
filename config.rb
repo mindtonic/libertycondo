80270e95327bf40e08a11082fb26d40b5f7f546f
Jay Sanders <mindtonic@gmail.com>
2015-12-16 12:27:33 -0500

    First Draft

--- config.rb
+++ config.rb
@@ -23,6 +23,8 @@ configure :development do
   activate :livereload
 end
 
+set :haml, { :ugly => true, :format => :html5 }
+
 ###
 # Helpers
 ###
@@ -37,8 +39,8 @@ end
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 end
